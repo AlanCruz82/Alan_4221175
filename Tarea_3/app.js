133829c5067cf90afec0913c4bcf2a163b69ca67
AlanCruz82 <cruzportal4@gmail.com>
2024-08-28 21:22:57 -0600

    Funcion Suma

---
+++ Tarea_3/app.js
@@ -0,0 +1,8 @@
+let nUno = parseInt(prompt("Dame un número"));
+let nDos = parseInt(prompt("Dame otro número"));
+
+function sumar(pNum,sNum){
+    console.log("La suma de tus números es : " + (pNum + sNum));
+}
+
+sumar(nUno,nDos);
